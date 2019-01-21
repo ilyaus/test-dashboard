8918a4cbd15c0b73984c9c33fed45ecee9caebfa
Ilya Ushomirsky <ilya_us@yahoo.com>
2019-01-21 10:31:06 -0600

    Initial updates

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <template>
   <div id="app">
+    <!--
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="" class="username">Username</label>
+            <input type="text" class="form-control" v-model="user.username">
+          </div>
+          <div class="form-group">
+            <label for="" class="email">EMail</label>
+            <input type="text" class="form-control" v-model="user.email">
+          </div>
+          <button class="btn btn-primary" @click="submit">Submit</button>
+        </div>
+      </div>
+    </div>
   </div>
+
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        'username': '',
+        'email': ''
+      }
+    };
+  },
+  methods: {
+    'submit': ''
+  }
+}
+</script>
+
 
 <style>
 #app {
